a473a84bff535771ef700e006c402bfabc6113fc
Patrick Snape <patricksnape@gmail.com>
2014-05-01 11:28:15 +0000

    Change name for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-setup(name='Sphinx map xrefrole',
+setup(name='sphinxmapxrefrole',
       version='0.1.0',
       description='Sphinx mapping role',
       author='Patrick Snape',
