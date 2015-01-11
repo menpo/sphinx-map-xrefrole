8adaebadded871c476502972936fcc245fd41a53
Patrick Snape <patricksnape@gmail.com>
2015-01-11 19:23:53 +0000

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='sphinxmapxrefrole',
-      version='0.1.0',
+      version='0.2.0',
       description='Sphinx mapping role',
       author='Patrick Snape',
       author_email='patricksnape@gmail.com',
